02ee7f638b7254d3752549da2d2e1e49b2cb0604
Zidan <b93605079@g.ntu.edu.tw>
2020-07-02 13:23:05 +0800

    開始先版型

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="#" type="image/x-icon">
     <title>Zidan-Web Resume</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="bootstrap.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
@@ -14,16 +14,38 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V" crossorigin="anonymous">
     <!-- customCSS -->
     <link rel="stylesheet" href="./css/style.css">
+    
 </head>
 <body>
 <!-- 準備include header-->
     <header>
-        <a href="index.php">
-            <div>
-                <img src="" alt="">
-                <p>Zidan</p>
-            </div>
-        </a>
+    <header class="bg-dark fixed-top">
+    <nav class="navbar navbar-expand-md navbar-dark container">
+      <a class="navbar-brand" href="index.php">ZidanResume</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#zinav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="zinav">
+        <ul class="navbar-nav ">
+          <li class="nav-item active">
+            <a class="nav-link" href="#lokiroom"><i class="fas fa-bed mr-2"></i>About Me</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#lokifacility"><i class="fas fa-coffee mr-2"></i>My Resume</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#lokifood"><i class="fas fa-pizza-slice mr-2"></i>餐飲美食</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#lokitrans"><i class="fas fa-map-marker-alt mr-2"></i>交通資訊</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#lokicontact"><i class="fas fa-phone-square-alt mr-2"></i>聯絡我們</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
     </header>
 
 <!-- Main section -->
